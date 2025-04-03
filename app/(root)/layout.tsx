87cc7a058ce5777399a0ecfcf8df791927d04a05
Rajawant Prajapati <rajawant.prajapati@freecharge.com>
2025-04-03 22:09:42 +0530

    Implement RightSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -10,9 +11,11 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex">
         <LeftSidebar />
 
-        <section className="px-6 pb-6 pt-36">
+        <section className="flex-1 px-6 pb-6 pt-36 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );
---
+++ components/navigation/RightSidebar.tsx
@@ -0,0 +1,69 @@
+import ROUTES from "@/constants/routes";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import TagCard from "../cards/TagCard";
+
+const hotQuestions = [
+  { _id: "1", title: "What is React virtual DOM?" },
+  { _id: "2", title: "How does useState work?" },
+  { _id: "3", title: "What is Redux Toolkit?" },
+  { _id: "4", title: "How to use useEffect?" },
+  { _id: "5", title: "What is useSelector in Redux?" },
+];
+
+const popularTags = [
+  { _id: "1", name: "react", questions: 100 },
+  { _id: "2", name: "javascript", questions: 200 },
+  { _id: "3", name: "typescript", questions: 150 },
+  { _id: "4", name: "nextjs", questions: 50 },
+  { _id: "5", name: "react-query", questions: 75 },
+];
+
+const RightSidebar = () => {
+  return (
+    <section className="background-light900_dark200 light-border sticky right-0 top-0  h-screen w-[350px] overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map(({ _id, title }) => (
+            <Link
+              href={ROUTES.PROFILE(_id)}
+              key={_id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">{title}</p>
+              <Image
+                src="/icons/chevron-right.svg"
+                alt="Chevron"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map(({ _id, name, questions }) => (
+            <TagCard
+              key={_id}
+              _id={_id}
+              name={name}
+              questions={questions}
+              showCount
+              compact
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
