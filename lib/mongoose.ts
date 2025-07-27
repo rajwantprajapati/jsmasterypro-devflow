9fefcb8ea9b59e0553d4f0224398e73468b27694
Rajwant Prajapati <rajwantprajapati5791@gmail.com>
2025-07-27 18:17:40 +0530

    Implement logging

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,46 +1,52 @@
-import mongoose, {Mongoose} from 'mongoose'
+import mongoose, { Mongoose } from "mongoose";
+import logger from "./logger";
 
 const MONGODB_URI = process.env.MONGODB_URL as string;
 
 if (!MONGODB_URI) {
-    throw new Error("MONGODB_URI not defined");
+  throw new Error("MONGODB_URI not defined");
 }
 
 interface MongooseCache {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 declare global {
-    // eslint-disable-next-line no-var
-    var mongoose: MongooseCache;
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache;
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async (): Promise<Mongoose> => {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, {
-            dbName: 'devflow',
-        }).then((mongoose) => {
-            return mongoose
-        }).catch((error) => {
-            console.error("Error in connecting to MongoDB:", error);
-
-            throw error;
-        })
-    }
-
-    cached.conn = await cached.promise;
+  if (cached.conn) {
+    logger.info("Using existing mongoose connenction");
     return cached.conn;
-}
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "devflow",
+      })
+      .then((result) => {
+        logger.info("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        logger.error("Error in connecting to MongoDB:", error);
+
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default dbConnect;
