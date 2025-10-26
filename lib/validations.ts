9ac252aeece800326441a788d8632e44226e3087
Rajwant Prajapati <rajwantprajapati5791@gmail.com>
2025-10-26 12:21:27 +0530

    Implement user's top tags

--- lib/validations.ts
+++ lib/validations.ts
@@ -208,3 +208,7 @@ export const GetUserQuestionsSchema = PaginatedSearchParamsSchema.extend({
 export const GetUserAnswersSchema = PaginatedSearchParamsSchema.extend({
   userId: z.string().min(1, { message: "User Id is required." }),
 });
+
+export const GetUserTagsSchema = z.object({
+  userId: z.string().min(1, { message: "User Id is required." }),
+});
