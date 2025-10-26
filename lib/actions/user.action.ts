cf6935d8d4bf833259601e0b4943b63e68f27357
Rajwant Prajapati <rajwantprajapati5791@gmail.com>
2025-10-26 10:45:37 +0530

    Implement profile links and Stats UI

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -76,7 +76,7 @@ export async function getUsers(
 
 export async function getUser(params: GetUserParams): Promise<
   ActionResponse<{
-    user: typeof User;
+    user: User;
     totalQuestions: number;
     totalAnswers: number;
   }>
